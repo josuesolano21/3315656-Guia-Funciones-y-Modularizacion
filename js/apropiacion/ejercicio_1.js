8d760b44fde44e1a9852b35f09df4e7d3feb5c55
juliandiaz-05 <juliandiazotavo@gmail.com>
2026-03-27 09:03:38 -0500

    Realice el Ejercicio 1 Apropiacion

--- js/apropiacion/ejercicio_1.js
+++ js/apropiacion/ejercicio_1.js
@@ -1,2 +1,28 @@
 /*Diseña un Algoritmo que muestre los números del 1 al 20, pero solo imprima
 los múltiplos de 3. */
+// Verifica si es múltiplo de 3
+function esMúltiplo(n) {
+  return n % 3 === 0;
+}
+
+// Muestra los múltiplos del 1 al 20
+function mostrarMúltiplos() {
+  for (let i = 1; i <= 20; i++) {
+    if (esMúltiplo(i)) {
+      console.log(i);
+    }
+  }
+}
+
+// Ejecutar
+mostrarMúltiplos();
+```
+
+**Salida esperada:**
+```
+3
+6
+9
+12
+15
+18
