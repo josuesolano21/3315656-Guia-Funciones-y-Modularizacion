c9b89eb6841dce412322d0d9a49dc60a2a65a7ec
josuesolano21 <josues230@hotmail.com>
2026-03-27 10:29:55 -0500

    Ejercicio 7 de Apropiación Realizado

--- js/apropiacion/ejercicio_7.js
+++ js/apropiacion/ejercicio_7.js
@@ -1,2 +1,29 @@
 /*Diseña un algoritmo que sume los números del 1 al 100, pero interrumpa el
 ciclo si encuentra un número mayor a 90 que sea par.*/
+
+// Verifica si debe interrumpir el ciclo
+function debeInterrumpir(n) {
+  return n > 90 && n % 2 === 0;
+}
+
+// Muestra el resultado final
+function mostrarResultado(suma, n) {
+  console.log(`Ciclo interrumpido en: ${n}`);
+  console.log(`Suma total: ${suma}`);
+}
+
+// Función principal
+function sumarNumeros() {
+  let suma = 0;
+
+  for (let i = 1; i <= 100; i++) {
+    if (debeInterrumpir(i)) {
+      mostrarResultado(suma, i);
+      break;
+    }
+    suma += i;
+  }
+}
+
+// Ejecutar
+sumarNumeros();
