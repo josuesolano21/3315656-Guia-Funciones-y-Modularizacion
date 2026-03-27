0ec2cb609346dd1ef49933e3eaf9ead338d7e35f
sararojas3315656 <saresro04@gmail.com>
2026-03-26 20:13:14 -0500

    corrección ejercicio #1 de transferencia.

--- js/transferencia/ejercicio_1.js
+++ js/transferencia/ejercicio_1.js
@@ -6,22 +6,33 @@ condicionales para verificar qué números cumplen la condición de ser primos.*
 Crear un programa que: muestre todos los números primos entre 1 y 50. Utilizar ciclos y
 condicionales para verificar qué números cumplen la condición de ser primos.*/
 
-for (let num = 1; num <= 50; num++) {
-  let primo = true;
-  if (num < 2) {
-    primo = false;
-  }
+function Primos() {
+  for (let num = 1; num <= 50; num++) {
+    let primo = true;
 
-  for (let divisor = 2; divisor < num; divisor++) {
-    if (num % divisor === 0) {
-      primo = false; 
+    if (num < 2) {
+      primo = false;
     }
-  }
-  if (primo) {
-    console.log(num);
+
+    for (let divisor = 2; divisor < num; divisor++) {
+      if (num % divisor === 0) {
+        primo = false;
+      }
+    }
+
+    if (primo) {
+      console.log(num);
+    }
   }
 }
 
-/* Ciclo FOR, repite el bloque de código varias veces, da un inicio, una condición y una actualización. 
-++, le suma 1 a una variable ej: número = número + 1 
-% (módulo)  devuelve el residuo de una división. */
+Primos();
+/* for principal recorre todos los números del 1 al 50. En cada vuelta
+Al inicio de cada número se asume que es primo si se pone en true. Si se demuestra que no lo es, da false. Se reinicia en cada vuelta.
+
+if (num < 2) {
+  primo = false;
+}   Los números 0 y 1 no son primos por definición
+El for anidado prueba dividir num entre todos los números desde 2 hasta justo antes de este.
+
+El % devuelve el residuo de una división. el % divisor === 0 significa que fue exacta
